ec5a2906529a7bdad6a7c6a110ae58711d056f58
sumonahmedjubayer121997 <jubayersumonahmed@gmail.com>
2025-07-29 00:07:29 +0100

    Do sum from a number until it getting 0 final verisojn

--- Beginners/SQ3/sq3.py
+++ Beginners/SQ3/sq3.py
@@ -1,15 +1,37 @@
 # -*- coding: utf-8 -*-
+# def get_total_sum(s):
+#     total = 0
+#     for i in range(s, 0, -1):
+#         total+=i
+#     return total
+
+# def get_total_sum(s):
+#     number_value = int(s)
+#     total = 0
+#     for i in range(1,number_value+1):
+#         total += i 
+#     return total
+
+
 def get_total_sum(s):
+    get_num = int(s)
     total = 0
-    for i in range(s, 0, -1):
-        total+=i
+    while get_num>0:
+        total += get_num
+        get_num-=1 #also can do get_num-=1 also can do get_num = get_num-1
     return total
 
+    
 
 def main():
-    input_number = int(input(""))
-    get_sum = get_total_sum(input_number)
-    print(get_sum)
+    try:
+        input_number = input("")
+        get_sum = get_total_sum(input_number)
+        print(get_sum)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+    
 
 if __name__ == '__main__':
     main()
